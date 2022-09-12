d06aae1cd1e87ae8ce12f039df4f9a05e901e736
Mir1001 <matej.crepinsek@gmail.com>
2022-09-12 18:08:39 +0200

    update script

--- machines/gb/configure.nix
+++ machines/gb/configure.nix
@@ -4,10 +4,12 @@
   imports = [
     ../../modules/image.nix
     ../../modules/base.nix
+    ../../modules/docker-host.nix
+    ../../modules/docker-auto-clean.nix 
     ../../users/root.nix
     ../../users/user.nix
     ../../users/gb.nix
   ];
 
-  networking.firewall.allowedTCPPorts = [22 9100];
+  networking.firewall.allowedTCPPorts = [22 5050 9100];
 }

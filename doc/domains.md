f6de60fe0d4db7f2443c3acd0564d8c49a70a2a9
Mir1001 <matej.crepinsek@gmail.com>
2022-09-12 15:25:57 +0200

    Update config for collab and gb

--- doc/domains.md
+++ doc/domains.md
@@ -18,3 +18,5 @@
 | gemini.lpm.feri.um.si      | 164.8.230.208 |
 | network.lpm.feri.um.si     | 164.8.230.211 |
 | bioma2022.um.si            | 164.8.230.208 |
+| collab.lpm.feri.um.si      | 164.8.230.208 |
+| gb.lpm.feri.um.si          | 164.8.230.208 |
--- machines/collab/configure.nix
+++ machines/collab/configure.nix
@@ -4,10 +4,15 @@
   imports = [
     ../../modules/image.nix
     ../../modules/base.nix
+    ../../modules/docker-host.nix
+    ../../modules/docker-auto-clean.nix 
+    ../../modules/backup.nix
     ../../users/root.nix
     ../../users/user.nix
     ../../users/collab.nix
   ];
 
-  networking.firewall.allowedTCPPorts = [22 9100];
+  environment.systemPackages = [pkgs.docker-compose];
+  networking.firewall.allowedTCPPorts = [22 5050 9100];
+
 }

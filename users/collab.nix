b98abf79f7da9399ccd8761d05e4746792494eeb
Žiga Leber <zigaleber7@gmail.com>
2022-10-05 19:51:48 +0200

    Update collab.nix

--- users/collab.nix
+++ users/collab.nix
@@ -3,6 +3,7 @@
   users.users.collab = {
     isNormalUser = true;
     description = "Collaboration platform user";
+    extraGroups = ["docker"];
     openssh.authorizedKeys.keys = with import ../ssh-keys.nix; [mario ziga matej];
   };
 }

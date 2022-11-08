af1fb55fe41d4387aa5225f8d8b2398ac0dca468
Žiga Leber <zigaleber7@gmail.com>
2022-11-08 12:16:18 +0100

    Use matrix

--- machines/deploy.sh
+++ machines/deploy.sh
@@ -10,21 +10,23 @@ deploy() {
     nixos-rebuild switch --upgrade --target-host "$host" -I nixpkgs="$pkgs" -I nixos-config="$config" --show-trace
 }
 
-deploy green './green/configuration.nix'
-deploy bastion './bastion/configure.nix'
-deploy gateway './gateway/configure.nix'
-deploy spum-platform './spum-platform/configure.nix'
-deploy spum-mqtt './spum-mqtt/configure.nix'
-#deploy spum-docker-registry './spum-docker-registry/configure.nix'
-deploy builder './builder/configure.nix'
-deploy grades './grades/configure.nix'
-deploy ps './ps/configure.nix'
-deploy esp './esp/configure.nix'
-deploy usatour './usatour/configure.nix'
-deploy calendar './calendar/configure.nix'
-deploy prometheus './prometheus/configure.nix'
-deploy bioma './bioma/configure.nix'
-#deploy mihaelhpc './mihaelhpc/configure.nix' 'https://github.com/NixOS/nixpkgs/archive/7e9b0dff974c89e070da1ad85713ff3c20b0ca97.tar.gz'
-deploy gb './gb/configure.nix'
-deploy ears './ears/configure.nix'
-deploy collab './collab/configure.nix'
+machine=${1:?Machine not specified}
+
+deploy "$machine" "./$machine/configure.nix"
+
+#deploy gateway './gateway/configure.nix'
+#deploy spum-platform './spum-platform/configure.nix'
+#deploy spum-mqtt './spum-mqtt/configure.nix'
+##deploy spum-docker-registry './spum-docker-registry/configure.nix'
+#deploy builder './builder/configure.nix'
+#deploy grades './grades/configure.nix'
+#deploy ps './ps/configure.nix'
+#deploy esp './esp/configure.nix'
+#deploy usatour './usatour/configure.nix'
+#deploy calendar './calendar/configure.nix'
+#deploy prometheus './prometheus/configure.nix'
+#deploy bioma './bioma/configure.nix'
+##deploy mihaelhpc './mihaelhpc/configure.nix' 'https://github.com/NixOS/nixpkgs/archive/7e9b0dff974c89e070da1ad85713ff3c20b0ca97.tar.gz'
+#deploy gb './gb/configure.nix'
+#deploy ears './ears/configure.nix'
+#deploy collab './collab/configure.nix'

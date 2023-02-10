a437b5155458e755f55393c3697e8cd3e0030060
Žiga Leber <zigaleber7@gmail.com>
2023-02-10 21:28:47 +0100

    Convert repository to flake, add sso-test service

--- machines/sso-test.l/configuration.nix
+++ machines/sso-test.l/configuration.nix
@@ -8,5 +8,18 @@
     ../../users/sso-test.nix
   ];
 
-  networking.firewall.allowedTCPPorts = [22];
+  networking.firewall.allowedTCPPorts = [22 8080];
+
+  services.ssoTest = {
+    user = "sso-test";
+    port = 8080;
+    domain = "sso-test.lpm.feri.um.si";
+    sessionSecret = pkgs.writeText "sessionSecret" "secret";
+    samlCertificate = ./saml.crt;
+    samlCertificateKey = config.age.secrets.saml-key.path;
+    idpMetadata = builtins.fetchurl {
+      url = "https://ds.aai.arnes.si/metadata/aai.arnes.si.sha256.xml";
+      sha256 = "0w6ww213cjzxlya8bb47gdx8vqcp1wif6ycsymlxggvi1i8gc87i";
+    };
+  };
 }

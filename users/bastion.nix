c90c9479c3ff1578b69134dcc5be2ee50bfbeddd
Marko Šmid <78045671+SmidMarko@users.noreply.github.com>
2023-07-04 14:03:18 +0200

    Update bastion.nix
    Add marko permission

--- users/bastion.nix
+++ users/bastion.nix
@@ -5,7 +5,7 @@
     description = "Bastion user";
     shell = "${pkgs.shadow}/bin/nologin";
     openssh.authorizedKeys.keys = with import ../ssh-keys.nix; [
-      mario ziga matej miha dragana bostjan server-actions bioma-actions bass-actions matej-actions dragana-actions
+      mario ziga matej miha dragana bostjan marko server-actions bioma-actions bass-actions matej-actions dragana-actions
     ];
   };
 }

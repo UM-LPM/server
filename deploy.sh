dab635f6c27fe22d1aff0fb537056e92fdebdf21
Žiga Leber <zigaleber7@gmail.com>
2023-07-09 17:12:06 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@
 deploy() {
     local host=$1
     local config=$2
-    nixos-rebuild switch --target-host "$host" --flake ".#$host"  
-}
+    nix run "nixpkgs#nixos-rebuild" switch --target-host "$host" --flake ".#$host"  
+
 
 machine=${1:?Machine not specified}
 

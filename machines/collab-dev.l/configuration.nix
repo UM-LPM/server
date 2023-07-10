853dc9e7d91281cab3e48411ca9aa25bcabb65ec
Žiga Leber <zigaleber7@gmail.com>
2023-07-10 12:10:20 +0200

    Add missing config

---
+++ machines/collab-dev.l/configuration.nix
@@ -0,0 +1,19 @@
+{config, pkgs, ...}:
+
+{
+  imports = [
+    ../../modules/image.nix
+    ../../modules/base.nix
+    ../../users/root.nix
+    ../../users/rescue.nix
+    ../../users/collab.nix
+  ];
+
+  networking.firewall.allowedTCPPorts = [22 80 8080 9100];
+
+  noo.services.collab = {
+    enable = true;
+    jwtSecret = "mysecret";
+    adminDefaultPassword = "myadmindefaultpass";
+  };
+}

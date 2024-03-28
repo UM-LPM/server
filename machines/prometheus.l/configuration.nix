6932c8ed055ec226c58418e3518c400120d1bf7e
Žiga Leber <zigaleber7@gmail.com>
2024-03-28 15:25:37 +0100

    Update configuration.nix

--- machines/prometheus.l/configuration.nix
+++ machines/prometheus.l/configuration.nix
@@ -2,21 +2,24 @@
 
 let
   targets = [
-    "localhost:9100"
-    "gateway:9100"
-    "builder:9100"
-    "grades:9100"
-    "spum-platform:9100"
-    "spum-mqtt:9100"
+    #"localhost:9100"
+    "gateway.l:9100"
+    "bastion.l:9100"
+    "builder.l:9100"
+    "prometheus.l:9100"
+    "runner1.l:9100"
+    "calendar.l:9100"
+
+    #"grades:9100"
+    #"spum-platform:9100"
+    #"spum-mqtt:9100"
     /* "spum-docker-registry:9100" */
-    "ps:9100"
-    "esp:9100"
-    "usatour:9100"
-    "calendar:9100"
-    "win10hpc:9182"
-    "prometheus:9100"
-    "mihaelhpc:9100"
-    "green:9100"
+    #"ps:9100"
+    #"esp:9100"
+    #"usatour:9100"
+    #"win10hpc:9182"
+    #"mihaelhpc:9100"
+    #"green:9100"
     "_gateway:9100" /* the host machine */
   ];
 in

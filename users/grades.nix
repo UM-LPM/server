5362e1032741ffdf5f956f56dd2d0e3e2283f816
Žiga Leber <mail+github@zigaleber.com>
2024-07-18 16:34:27 +0200

    Update grades.nix

--- users/grades.nix
+++ users/grades.nix
@@ -3,6 +3,6 @@
   users.users.grades = {
     isNormalUser = true;
     description = "Grading app";
-    openssh.authorizedKeys.keys = with import ../ssh-keys.nix; [ziga matej mario domen];
+    openssh.authorizedKeys.keys = with import ../ssh/users.nix; [ziga matej mario domen];
   };
 }

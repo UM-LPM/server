82e18c0282730c2739fd9bdf2044d4d016a04ec8
Žiga Leber <mail+github@zigaleber.com>
2025-01-25 16:11:14 +0100

    Load courses.json from service

--- machines/catalog-view.l/view.nix
+++ machines/catalog-view.l/view.nix
@@ -1,11 +1,15 @@
-{lib, stdenvNoCC, fetchFromGitHub, ruby, bundlerEnv, nodejs}:
+{lib, stdenvNoCC, fetchurl, fetchFromGitHub, ruby, bundlerEnv, nodejs}:
 
 let
   src = fetchFromGitHub {
     owner = "UM-LPM";
     repo = "short-courses";
-    rev = "2574eb9e8c50c21efe51f63d65e76eb7026b5a54";
-    hash = "sha256-3Eu6hJhMdqxxdH0QjT6/KZ4fBLLc317f3QY0R6Yq8Q4=";
+    rev = "6a6a82e5baedce00f030cbb7f6a6d89afa6be57f";
+    hash = "sha256-Gg6J5Go4mE/iNUc2Uyd2fu19cob/eYvhRARDVl9Xhp0=";
+  };
+  courses = fetchurl {
+    url = "https://catalog.pmd.lpm.feri.um.si/api/course";
+    hash = "sha256-p+THcJJl5qAtltQUOb2FULZpkxXwIb3QM5KoeWrywVc=";
   };
   jekyll_env = bundlerEnv {
     name = "jekyll_env";
@@ -24,6 +28,7 @@ stdenvNoCC.mkDerivation {
   ];
 
   buildPhase = ''
+    cp ${courses} _data/courses.json
     bundle exec jekyll build
   '';
 

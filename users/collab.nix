a1b6940cc7c8d5c5498166f785cc021e4ab022a7
matejc <matej.crepinsek@um.si>
2025-03-07 14:27:56 +0100

    Update collab.nix
    add user cvetanka collab

--- users/collab.nix
+++ users/collab.nix
@@ -4,6 +4,6 @@
     isNormalUser = true;
     description = "Collaboration platform user";
     extraGroups = ["docker"];
-    openssh.authorizedKeys.keys = with import ../ssh/users.nix; [mario ziga matej marko merisa];
+    openssh.authorizedKeys.keys = with import ../ssh/users.nix; [mario ziga matej marko cvetanka];
   };
 }

b49db735a2d699134a3a43235ea332b91b926227
Žiga Leber <mail+github@zigaleber.com>
2025-04-22 12:54:22 +0200

    Update nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-24.05;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-24.11;
     agenix.url = github:ryantm/agenix;
     sso-test.url = github:UM-LPM/sso-test;
     grades.url = github:UM-LPM/grades;

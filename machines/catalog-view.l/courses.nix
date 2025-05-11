c9a68daf13d764aae46d4c82d8333a15d0a27638
Žiga Leber <mail+github@zigaleber.com>
2025-05-11 21:28:03 +0200

    Fix download url

--- machines/catalog-view.l/courses.nix
+++ machines/catalog-view.l/courses.nix
@@ -1,11 +1,12 @@
-{fetchurl}:
+{lib, fetchurl}:
 let
+  catalog = "3b88bf36-eb8b-4c9f-bd29-545451665e87";
   revision = builtins.readFile ./revision;
 in
 fetchurl {
   urls = [
-    "http://catalog-manage.l:8080/api/course/short-courses/3b88bf36-eb8b-4c9f-bd29-545451665e87?timestamp=${revision}"
-    "https://scms.catalog.lpm.rwx.si/vse/api/course/short-courses/3b88bf36-eb8b-4c9f-bd29-545451665e87?timestamp=${revision}"
+    "http://catalog-manage.l:8080/api/course/short-courses/${catalog}?timestamp=${revision}"
+    "https://scms.catalog.lpm.rwx.si/api/course/short-courses/${catalog}?timestamp=${revision}"
   ];
-  hash = "sha256-dDXsCEmmWr4bMmLUvAhMkVNHs1FW8nZRPEZeNZ7aSos=";
+  hash = "sha256-stBcMVbk+HcPp6jMPa7L6JkIIxRrEnfP6r2rPOQwMag=";
 }

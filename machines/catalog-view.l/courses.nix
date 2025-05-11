94238d8b3468c3ce84df7fb5d3332b6bc7c96c1a
lpm-automaton <lpm-automaton@protonmail.com>
2025-05-11 19:05:44 +0000

    Update courses

--- machines/catalog-view.l/courses.nix
+++ machines/catalog-view.l/courses.nix
@@ -7,5 +7,5 @@ fetchurl {
     "http://catalog-manage.l:8080/api/course/short-courses/3b88bf36-eb8b-4c9f-bd29-545451665e87?timestamp=${revision}"
     "https://scms.catalog.lpm.rwx.si/vse/api/course/short-courses/3b88bf36-eb8b-4c9f-bd29-545451665e87?timestamp=${revision}"
   ];
-  hash = "sha256-pO6BJsHVgue6mDtQNgTXIeHMGMTSTzRvnUOaoHc4N3c=";
+  hash = "sha256-dDXsCEmmWr4bMmLUvAhMkVNHs1FW8nZRPEZeNZ7aSos=";
 }

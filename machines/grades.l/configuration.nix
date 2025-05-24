94f9ffa3b01e52ce9ce7e0f40cd7d222632fd76a
DomenOnassis <domen.onassis@gmail.com>
2025-05-24 18:30:41 +0200

    Added external secrets for collab and grades

--- machines/grades.l/configuration.nix
+++ machines/grades.l/configuration.nix
@@ -9,6 +9,13 @@
     ../../users/grades.nix
   ];
 
+  age.secrets."grades-external-secrets" = {
+    file = ../../secrets/grades-external-secrets.age;
+    mode = "600";
+    owner = "grades";
+    group = "users";
+  };
+
   environment.systemPackages = [];
   networking.firewall.allowedTCPPorts = [22 80 3003 9100];
 
@@ -17,5 +24,6 @@
     frontend.enable = true;
     backend.enable = true;
     database.enable = true;
+    externalSecretsFile = config.age.secrets."grades-external-secrets".path;
   };
 }

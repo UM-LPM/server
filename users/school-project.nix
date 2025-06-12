4fbab43b12d55f41715d7bcbb67d2bab2545fdf4
matejc <matej.crepinsek@um.si>
2025-06-12 20:26:37 +0200

    Update school-project.nix
    Add root access

--- users/school-project.nix
+++ users/school-project.nix
@@ -3,6 +3,7 @@
   users.users.schoolproject = {
     isNormalUser = true;
     description = "School project user";
+    extraGroups = ["wheel"];
     openssh.authorizedKeys.keys = with import ../ssh/users.nix; [mario ziga matej];
   };
 }

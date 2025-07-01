df73579091d9f3a5fea1125a411e560e09c5e0ab
lpm-automaton <lpm-automaton@protonmail.com>
2025-07-01 13:22:14 +0000

    Update summer schools

--- machines/catalog-summer-schools.l/view.nix
+++ machines/catalog-summer-schools.l/view.nix
@@ -10,8 +10,8 @@ mkView {
   src = fetchFromGitHub {
     owner = "UM-LPM";
     repo = "short-courses";
-    rev = "c4cbbde4d0aa5db03b84ddc36cfa5996137e1324";
-    hash = "sha256-svRsQ+CiIWOJj0OVNG8+8uL/cWtQkWukcD511Cixn9E=";
+    rev = "af18c46afe39e93310cdc2f3e163aaf818ad3c33";
+    hash = "sha256-Epm2C6V1xchD1gsix1nQcV6/jKbn4JzUn49buX/xAP0=";
   };
   courses = mkCourses {
     inherit catalog revision hash;

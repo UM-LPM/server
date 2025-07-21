5a2c879c22522cd622b76f10728ecde288b2886f
DomenOnassis <domen.onassis@gmail.com>
2025-07-21 18:01:18 +0200

    Added external secrets to all collab machines

--- machines/collab-vr.l/configuration.nix
+++ machines/collab-vr.l/configuration.nix
@@ -11,11 +11,19 @@
 
   networking.firewall.allowedTCPPorts = [22 80 8080 9100];
 
+  age.secrets."collab-external-secrets" = {
+    file = ../../secrets/collab-external-secrets.age;
+    mode = "600";
+    owner = "collab";
+    group = "users";
+  };
+
   noo.services.collab = {
     enable = true;
     host = "0.0.0.0";
     package = pkgs.collab-vr;
     jwtSecret = "mysecret";
+    oidcIssuer = "https://dev.login.lpm.feri.um.si/oidc";
     oidcIssuer = "https://login.lpm.feri.um.si/oidc";
     adminDefaultPassword = "myadmindefaultpass";
   };

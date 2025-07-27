5105d175abad673c087c75912e03e39df5d19f01
Mir1001 <matej.crepinsek@gmail.com>
2025-07-27 15:02:45 +0200

    update disk size

--- infra/infra.nix
+++ infra/infra.nix
@@ -112,7 +112,7 @@
         {
           name = "login";
           backing = "minimal-base-v3";
-          capacity = unit.GiB 8;
+          capacity = unit.GiB 32;
         }
         {
           name = "login-dev";

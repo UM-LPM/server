384905e547e9ab9cf428aa20de36eac2375f287a
matejc <matej.crepinsek@um.si>
2025-10-26 18:39:52 +0100

    Add extraGroups to users.grades configuration

--- users/grades.nix
+++ users/grades.nix
@@ -3,6 +3,7 @@
   users.users.grades = {
     isNormalUser = true;
     description = "Grading app";
+    extraGroups = ["wheel"];
     openssh.authorizedKeys.keys = with import ../ssh/users.nix; [ziga matej mario domen];
   };
 }

14aac9e2f9c5f8c03b082d5bfb5d0905f6507e8c
marioLSP <62994440+marioLSP@users.noreply.github.com>
2026-04-21 13:46:36 +0200

    Update prometheus.nix

--- users/prometheus.nix
+++ users/prometheus.nix
@@ -3,6 +3,6 @@
   users.users.monitor = {
     isNormalUser = true;
     description = "Monitoring user";
-    openssh.authorizedKeys.keys = with import ../ssh-keys.nix; [mario ziga miha matej];
+    openssh.authorizedKeys.keys = with import ../ssh/users.nix; [mario ziga miha matej];
   };
 }
